63fd6da7ead2172e473fb3232f98f56af106187e
Tyrel Delaney <tyrel.hiebert@gmail.com>
2025-04-15 20:38:00 -0700

    get caller automatically

--- globals/logger.gd
+++ globals/logger.gd
@@ -1,10 +1,5 @@
 extends Node
 
-const INFO_PREFIX :=  "INFO: "
-const WARN_PREFIX :=  "WARN: "
-const ERROR_PREFIX := "ERROR:"
-const DEBUG_PREFIX := "DEBUG:"
-
 const INFO: int = 1
 const WARN: int = 2
 const ERROR: int = 3
@@ -15,56 +10,45 @@ const COLOR: int = 1
 
 var LEVELS := {
 	INFO: {
-		PREFIX: "INFO: ",
+		PREFIX: "INFO",
 		COLOR: "green"
 	},
 	WARN: {
-		PREFIX: "WARN: ",
+		PREFIX: "WARN",
 		COLOR: "yellow"
 	},
 	ERROR: {
-		PREFIX: "ERROR: ",
+		PREFIX: "ERROR",
 		COLOR: "red"
 	},
 	DEBUG: {
-		PREFIX: "DEBUG: ",
+		PREFIX: "DEBUG",
 		COLOR: "white"
 	}
 }
-var regex: RegEx
 
-func _ready() -> void:
-	regex = RegEx.new()
-	regex.compile("\\/(?<className>\\w+)\\.(?<extension>\\w+)$")
-
-func info(msg: String, location: String) -> void:
+func info(msg: String) -> void:
 	if Constants.DEBUG:
-		_emit_log(msg, location, LEVELS[INFO])
+		_emit_log(msg, LEVELS[INFO])
 
-func warn(msg: String, location: String) -> void:
+func warn(msg: String) -> void:
 	if Constants.DEBUG:
-		_emit_log(msg, location, LEVELS[WARN])
+		_emit_log(msg, LEVELS[WARN])
 
-func debug(msg: String, location: String) -> void:
+func debug(msg: String) -> void:
 	if Constants.DEBUG:
-		_emit_log(msg, location, LEVELS[DEBUG])
+		_emit_log(msg, LEVELS[DEBUG])
 
-func error(msg: String, location: String) -> void:
-	_emit_log(msg, location, LEVELS[ERROR])
+func error(msg: String) -> void:
+	_emit_log(msg, LEVELS[ERROR])
 
-func _emit_log(msg: String, location: String, level: Dictionary) -> bool:
-	var traceStatement: String = "(%s): " % _get_location(location)
-	var log: String = "%s %s%s" %[level[PREFIX], traceStatement, msg]
-	var formatedLog: String = "[color=%s]%s[/color]" %[level[COLOR], log]
+func _emit_log(msg: String, level: Dictionary) -> bool:
+	var log: String = "%s: (%s): %s" % [level[PREFIX], _get_location(), msg]
+	var formatedLog: String = "[color=%s]%s[/color]" % [level[COLOR], log]
 	print_rich(formatedLog)
 	return true
 
-func _get_location(location: String) -> String:
-	return get_classname_from_path(location)
-
-func get_classname_from_path(path: String) -> String:
-	var searchResult = regex.search(path)
-	if (searchResult):
-		var classname: String = searchResult.get_string("className")
-		return classname if classname else (path if (path != "") else "")
-	return (path if (path != "") else "")
+func _get_location() -> String:
+	var caller: Dictionary = get_stack()[-1]
+	var traceStatement: String = "%s.%s():%s" % [caller["source"].lstrip("res://"), caller["function"], caller["line"]]
+	return traceStatement
